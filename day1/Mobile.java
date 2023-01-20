902479647634d3271f482d018d3dee985e563ec7
sangeethapriya-97 <123191937+sangeethapriya-97@users.noreply.github.com>
2023-01-20 22:11:19 +0530

    Add files via upload
    Day1

---
+++ day1/Mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mobileNo = 1234567890;
+		boolean isIphone = false;
+		char brand ='i';
+	    String brandName ="Iphone";
+	    System.out.println("Mobile Number="+ mobileNo);
+	    System.out.println(isIphone);
+	    System.out.println("Brand Name" +brandName);
+				
+
+	}
+
+}
